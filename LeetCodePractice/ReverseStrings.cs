d0626016df8a61072fe26d5e024957cb616a88e0
asiii <asiii@ASIF>
2024-11-27 21:12:39 +0600

    Add project files.

---
+++ LeetCodePractice/ReverseStrings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodePractice
+{
+    internal class ReverseStrings
+    {
+        
+            public void ReverseString(char[] s)
+            {
+
+                int first = 0;
+                int last = s.Length - 1;
+
+                while (first < last)
+                {
+                    char revrsString = s[first];
+                    s[first] = s[last];
+                    s[last] = revrsString;
+                    first++;
+                    last--;
+                }
+            }
+        
+    }
+}
